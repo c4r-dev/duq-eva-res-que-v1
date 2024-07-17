4e97ced7d73b9d76fafb603b2352741a6924f66a
David Fischer <fiscd@seas.upenn.edu>
2024-07-17 11:47:55 -0400

    erq summary

--- app/summary/page.jsx
+++ app/summary/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import SummaryCalc from "@/app/components/summaryCalc";
+import SummaryCalc from "@/components/summaryCalc";
 
 export default function Summary() {
   return <SummaryCalc />;
