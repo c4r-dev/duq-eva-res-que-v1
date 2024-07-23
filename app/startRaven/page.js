7cbf3364cd57d61376aa117913f188c189132ed8
David Fischer <fiscd@seas.upenn.edu>
2024-07-23 17:12:26 -0400

    suspense

--- app/startRaven/page.js
+++ app/startRaven/page.js
@@ -5,11 +5,16 @@ import { useRouter, useSearchParams } from "next/navigation";
 import Image from 'next/image';
 
 import Raven1 from '../assets/feedback-button-1.svg';
+import { Suspense } from "react";
 
 export default function ResQuesThree() {
 
-  const searchParams = useSearchParams()
-  const selected = searchParams.get("rqtool")
+  let selected = ''
+  function Search() {
+    const searchParams = useSearchParams()
+    selected = searchParams.get("rqtool")
+    return <input placeholder="Search..." />
+  }
 
   const router = useRouter()
 
@@ -21,6 +26,10 @@ export default function ResQuesThree() {
   return (
 
     <>
+      <Suspense>
+        <Search/>
+      </Suspense>
+
       <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <div>
           <h1>Formulating a Valid Research Question</h1>
